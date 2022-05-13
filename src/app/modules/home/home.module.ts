ff9c0adb8d5f654faf355e18d1d6a9fb4b832344
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-05-13 14:32:48 -0400

    project structure setted

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
